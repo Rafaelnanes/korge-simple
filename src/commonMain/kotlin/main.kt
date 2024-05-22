0e26f37ce5fd0f06237ccdf1a6aeb00818c9d963
Rafael Nanes <rafaelbraga_22@hotmail.com>
2024-05-22 14:46:13 +0100

    avoid collision

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,9 +1,12 @@
 import korlibs.event.*
 import korlibs.image.color.*
 import korlibs.korge.*
+import korlibs.korge.box2d.*
 import korlibs.korge.scene.*
 import korlibs.korge.view.*
+import korlibs.korge.view.collision.*
 import korlibs.math.geom.*
+import org.jbox2d.dynamics.*
 
 suspend fun main() = Korge(windowSize = Size(600, 600), backgroundColor = Colors["#2b2b2b"]) {
     val sceneContainer = sceneContainer()
@@ -14,13 +17,26 @@ suspend fun main() = Korge(windowSize = Size(600, 600), backgroundColor = Colors
 class MyScene : Scene() {
     override suspend fun SContainer.sceneMain() {
 
-        val playerSpeed = 10.0;
-
-        val player = solidRect(30, 30, color = Colors.BLUE)
+        val playerSpeed = 5.0;
+        val player = solidRect(30, 30, color = Colors.BLUE).registerBodyWithFixture(
+            type = BodyType.KINEMATIC,
+            density = 2,
+            friction = 0.01
+        )
         player.xy(0, 0)
-        player.addUpdater {
 
+        val enemy1 = solidRect(30, 30, color = Colors.RED).registerBodyWithFixture(
+            type = BodyType.STATIC,
+            friction = 0.2,
+            restitution = 0.2
+        )
+        enemy1.xy(100, 100)
+
+        player.addUpdater {
             val keys = views.input.keys
+            val lastX = x
+            val lastY = y
+
             if (keys[Key.D]) {
                 x += playerSpeed
             }
@@ -36,9 +52,13 @@ class MyScene : Scene() {
             if (keys[Key.S]) {
                 y += playerSpeed
             }
+
+            if (collidesWith(enemy1)) {
+                y = lastY
+                x = lastX
+            }
+
         }
-        val enemy1 = solidRect(30, 30, color = Colors.RED)
-        enemy1.xy(100, 100)
 
     }
 }
